5f6f7d3392603ca8aa4511a1bafe41278af3185f
Udaya Kiran S S <udaykiranss@icloud.com>
2018-07-30 23:55:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def welcome_message:
+def welcome:
     return "Welcome to Python API"
+
 
 if __name__ == '__main__':
     app.run(debug=1)
